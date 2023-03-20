eb172f8987df135896380a2d856c2df2755ccea0
sakibhosen98 <121639496+sakibhosen98@users.noreply.github.com>
2023-03-20 11:45:50 +0600

    rest-countries-by-react

---
+++ rest-countries-recap/js/countries.js
@@ -0,0 +1,53 @@
+const loadCountries = () =>{
+  fetch('https://restcountries.com/v3.1/all')
+  .then(res => res.json())
+  .then(data => displayCountries(data));
+}
+
+const displayCountries = countries =>{
+  console.log(countries[0]);
+  const countriesHTML = countries.map(country => getCountryHTML(country));
+  // console.log(countriesHTML[0])
+  const container = document.getElementById('countries');
+  container.innerHTML = countriesHTML.join(' ');
+}
+
+// Original system
+
+// const getCountryHTML = country => {
+//   return `
+//     <div class = "country">
+//       <h2>${country.name.common}</h2>
+//       <img src="${country.flags.png}" >
+//     </div>
+//   `
+// }
+
+// step - 2
+// const getCountryHTML = country => {
+//   // option 1
+//   const {name, flags} = country;
+//   return `
+//     <div class = "country">
+//       <h2>${name.common}</h2>
+//       <img src="${flags.png}" >
+//     </div>
+//   `
+// }
+
+// step - 3
+const getCountryHTML = ({name,flags, area, region}) => {
+  // option 1
+  return `
+    <div class = "country">
+      <h2>${name.common}</h2>
+      <p> Area:${area}</p>
+      <p> Continet: ${region}</p>
+      <img src="${flags.png}" >
+    </div>
+  `
+}
+
+
+
+loadCountries();
